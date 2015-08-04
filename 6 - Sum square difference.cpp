83b09a3cfd28ae07524bcfd8904919d8e34a719f
Monty Anderson <monty.linux@gmail.com>
2015-08-04 12:06:21 +0100

    problem 6

---
+++ 6 - Sum square difference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    long int sum = 0,
+        square = 0,
+        max = 100;
+
+    for(int i = 1; i <= max; i++) {
+        sum += i * i;
+        square += i;
+    }
+
+    square *= square;
+
+    cout << square - sum << endl;
+}
